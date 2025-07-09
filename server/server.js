1f186e677bbc4f3e0cddad5ec5daf4bd3d4b8f48
Aabhash-Khemka <aabhashkhemka@gmail.com>
2025-07-09 18:53:27 +0530

    Completed

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import connectDb from './configs/db.js';
+import adminRouter from './routes/adminRoutes.js';
+import blogRouter from './routes/blogRoutes.js';
+
+
+const app =express();
+
+await connectDb()
+
+app.use(cors())
+app.use(express.json())
+//Routes
+app.get('/',(req,res)=>res.send("API is working"))
+app.use('/api/admin',adminRouter)
+app.use('/api/blog',blogRouter)
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT , ()=>{
+    console.log('Server is Running on pORT ' + PORT)
+})
+
+export default app ;
