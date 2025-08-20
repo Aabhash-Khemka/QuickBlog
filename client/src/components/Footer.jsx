d56d8a72525512f7fff1e253bdc4be6b1b98d3c2
Aabhash-Khemka <aabhashkhemka@gmail.com>
2025-08-20 17:50:44 +0530

    Added description in the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className='flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500'>
                 <div>
                     <img src={assets.logo} alt="logo" className='w-32 sm:w-44' />
-                    <p className='max-w-[410px] mt-6'>Lorem ipsum dolor sit amet.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio explicabo doloribus nihil fugit, ea accusantium!
+                    <p className='max-w-[410px] mt-6'>QuickBlog: Read and share thoughtful articles, tutorials, and opinions. Create posts, explore topics, and join a friendly community.
                     </p>
                 </div>
                 <div className='flex flex-wrap justify-between w-full md:w-[45%] gap-5'>
